2c8c886ce0c4d03d59895d2d9d7496bc9d6e8e45
Pedro Henrique Bergental <phbergental@brazpine.com>
2021-12-06 00:04:54 -0300

    fix: ensure the tests pass

--- src/index.js
+++ src/index.js
@@ -23,20 +23,23 @@ app.post("/repositories", (request, response) => {
     likes: 0
   };
 
+  repositories.push(repository);
+
   return response.json(repository);
 });
 
 app.put("/repositories/:id", (request, response) => {
   const { id } = request.params;
-  const updatedRepository = request.body;
+  const { title, url, techs } = request.body;
+  const updatedRepository = { title, url, techs };
 
-  repositoryIndex = repositories.findindex(repository => repository.id === id);
+  repositoryIndex = repositories.findIndex(repository => repository.id === id);
 
   if (repositoryIndex < 0) {
     return response.status(404).json({ error: "Repository not found" });
   }
 
-  const repository = { ...repositories[repositoryIndex], ...updatedRepository };
+  const repository = Object.assign(repositories[repositoryIndex], updatedRepository);
 
   repositories[repositoryIndex] = repository;
 
@@ -48,7 +51,7 @@ app.delete("/repositories/:id", (request, response) => {
 
   repositoryIndex = repositories.findIndex(repository => repository.id === id);
 
-  if (repositoryIndex > 0) {
+  if (repositoryIndex < 0) {
     return response.status(404).json({ error: "Repository not found" });
   }
 
@@ -66,9 +69,12 @@ app.post("/repositories/:id/like", (request, response) => {
     return response.status(404).json({ error: "Repository not found" });
   }
 
-  const likes = ++repositories[repositoryIndex].likes;
+  const repository = repositories[repositoryIndex];
+  repository.likes += 1;
 
-  return response.json('likes');
+  return response.json({
+    likes: repository.likes
+  });
 });
 
 module.exports = app;
